ce88d10e1c543b17f3c777715680ce436efaefd6
Kyle Lowery <Loweryk1@ferris.edu>
2018-03-02 15:08:35 -0500

    Added vars to profile.php to store needed info to fill on-screen profile
    Added a list of vars to profile.php to store all information needed to build the user's profile in the html code.

--- profile.php
+++ profile.php
@@ -3,9 +3,34 @@
 include "includes/templates.php";
 include "includes/profileFunctions.php";
 
+// GET USER DATA:
+$user_id = 1;
+$first_name = "Firstname";
+$last_name = "Lastname";
+$title = "Title";
+$bio = "This is my bio.";
+
+$resume = "#";
+$contact_email = "#";
+$contact_twitter = "#";
+$contact_facebook = "#";
+
+$links1_name = "My DeviantArt";
+$links1_html = "#";
+
+$links2_name = "My Grandma's Etsy";
+$links2_html = "#";
+
+$links3_name = "My Tumblr";
+$links3_html = "#";
+
+// BUILD THE PAGE:
 beginPage("home", array("styles/profile.css"));
 mainMenu();
+
+
 ?>
+
         <div class="tray">
             <div class="carasol">
                     <?
@@ -17,11 +42,11 @@ mainMenu();
             <article>
                 <div class="creator">
                     <div class="avatar"><img src="imgs/placeholder-avatar1.jpg"></div>
-                    <h2>Namey McStudent</h2>
-                    <h3>Texture Artist</h3>
+                    <h2><?=($first_name . " " . $last_name) ?></h2>
+                    <h3><?=$title ?></h3>
                 </div>
                 <div class="bubble top">
-                    <p>My bio goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in </p>
+                    <p><? echo $bio; ?></p>
                     <div class="arrow top-left">
                         <div class="blocker"></div>
                         <div class="pointer"></div>
@@ -32,7 +57,7 @@ mainMenu();
                 <div class="split">
                     <div>My Resume</div>
                     <div>
-                        <a href="#" class="button">Download</a>
+                        <a href="<?=$resume ?>" class="button">Download</a>
                     </div>
                 </div>
                 <div class="hr"></div>
@@ -40,9 +65,9 @@ mainMenu();
                     <div>Contact Me</div>
                     <div>
                         <ul>
-                            <li><a href="#" class="work">Email</a></li>
-                            <li><a href="#" class="work">Twitter</a></li>
-                            <li><a href="#" class="work">Facebook</a></li>
+                            <li><a href="<?=$contact_email ?>" class="work">Email</a></li>
+                            <li><a href="<?=$contact_twitter ?>" class="work">Twitter</a></li>
+                            <li><a href="<?=$contact_facebook ?>" class="work">Facebook</a></li>
                         </ul>
                     </div>
                 </div>
@@ -51,9 +76,9 @@ mainMenu();
                     <div>My Links</div>
                     <div>
                         <ul>
-                            <li><a href="#" class="work">My DeviantArt</a></li>
-                            <li><a href="#" class="work">My Grandma’s Etsy</a></li>
-                            <li><a href="#" class="work">My Tumbler</a></li>
+                            <li><a href="<?=$links1_html ?>" class="work"><?=$links1_name ?></a></li>
+                            <li><a href="<?=$links2_html ?>" class="work"><?=$links2_name ?></a></li>
+                            <li><a href="<?=$links3_html ?>" class="work"><?=$links3_name ?></a></li>
                         </ul>
                     </div>
                 </div>
