e958dfaee29d8baccab6e81ca89ae07fb9f7779e
Allen G <gardeia@ferris.edu>
2018-06-22 01:01:55 -0400

    Prototype of new sign up page layout.
    Sign Up page is now a 1 row form with help text on the right hand side.

--- signup.php
+++ signup.php
@@ -49,45 +49,89 @@ mainMenu();
         <form class="signup" action="signup.php" method="post">
             
             <h1>Create an Account</h1>
-            <div class="input">
-                <div>
-                    <h1>Personal Information</h1>
-                    <h2>Username</h2>
-                        <input type ="text" id ="username" name="user-name">
-                    <h2>First Name</h2>
-                        <input type ="text" id ="firstname" name="user-firstname">
-                    <h2>Last Name</h2>
-                        <input type ="text" id ="lastname" name="user-lastname">
-                </div>
-                
-                <div>
-                        <h2>Title</h2>
-                        <input type ="text" id ="title" name="user-occupation">  
-                </div>
-                 
-                <div>
-                    <h1>Contact Information</h1>
-                    <h2>Email</h2>
-                        <input type ="text" id ="email" name="user-email">
-                    <h2>Confirm Email</h2>
-                        <input type ="text" id ="email" name="user-email">
-                </div>
-                
-                <div>
-                    <h1>Security</h1>
-                    <h2>Password</h2>
-                        <input type ="password" id ="password" name="user-password">
-                    <h2>Confirm Password</h2>
-                        <input type ="password" id ="password" name="user-password"> 
-                </div>
-
-            </div>
-            <div class="button" id="confirm-signup">
-                <button type="submit">Create Account</button>
-            </div>
+            
             <div class="haveText" >
-                <p>Already have an account? <a class="linkText" href="login.php">Login!</a></p>
+                    <p>Already have an account? <a class="linkText" href="login.php">Login!</a></p>
+                </div>
+            
+            <div>
+                <div class="input" id="formDiv">
+                    <section class="left">
+                        <h1>Personal Information</h1>
+                        <h2>Username</h2>
+                            <input type ="text" id ="username" name="user-name">
+                        <h2>First Name</h2>
+                            <input type ="text" id ="firstname" name="user-firstname">
+                        <h2>Last Name</h2>
+                            <input type ="text" id ="lastname" name="user-lastname">
+                    </section>
+                    
+                    <section class="right">
+                    <h1></h1>
+                        <p>Lorem ipsum dolor sit amet, mauris placerat ac sit cras, suspendisse convallis metus nonummy sapien, arcu conubia at, felis lectus, consectetuer lacus tempor.</p>
+                    </section>
+                    
+                    
+                </div>
+                
+                <div id="divider"></div>
+                
+                <div class="input" id="formDiv">
+                    <section class="left"> 
+                        <h1></h1>
+                        <h2>Title</h2>
+                        <input type ="text" id ="title" name="user-occupation"> 
+                    </section>  
+                    <section class="right">
+                        <h1></h1>
+                        <p>Are you a Programmer? Artist? Gamer? Dancer? Alien? TELL US ALREADY!</p>
+                    </section>
+                </div>
+                
+                <div id="divider"></div>
+                
+                <div class="input" id="formDiv">
+                    <section class="left">
+                        <h1>Contact Information</h1>
+                        <h2>Email</h2>
+                            <input type ="text" id ="email" name="user-email">
+                        <h2>Confirm Email</h2>
+                            <input type ="text" id ="email" name="user-email">
+                    </section>
+                    <section class="right">
+                        <h1></h1>
+                        <p>Lorem ipsum dolor sit amet, mauris placerat ac sit cras, suspendisse convallis metus nonummy sapien, arcu conubia at, felis lectus, consectetuer lacus tempor.</p>
+                    </section>
+                </div>
+                
+                <div id="divider"></div>
+                
+                <div class="input" id="formDiv">
+                    <section class="left">
+                        <h1>Security</h1>
+                        <h2>Password</h2>
+                            <input type ="password" id ="password" name="user-password">
+                        <h2>Confirm Password</h2>
+                            <input type ="password" id ="password" name="user-password"> 
+                    </section>
+                    <section class="right">
+                        <h1></h1>
+                        <p>Lorem ipsum dolor sit amet, mauris placerat ac sit cras, suspendisse convallis metus nonummy sapien, arcu conubia at, felis lectus, consectetuer lacus tempor.</p>
+                    </section>
+                </div>
+                <div id="divider"></div>
+            <div class="input" id="formDiv">
+                
+                <div class="button" id="confirm-signup">
+                    <button type="submit">Create Account</button>
+                </div>
+                
+                <div class="haveText" >
+                    <p>Already have an account? <a class="linkText" href="login.php">Login!</a></p>
+                </div>
             </div>
+            </div>
+            
         </form>
         <? } ?>
     </section>
