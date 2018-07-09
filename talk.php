f575b5667e8a70c13653786fad2052d2b1ef370e
Allen G <gardeia@ferris.edu>
2018-07-08 23:39:35 -0400

    Community Updates V2
    Implemented feedback. 
    -Links will now open a new tab/window
    -Added hover state settings
    -Text no longer baked into background.
    -Added a slight tint to backgrounds. Hard to give a solid background color that fits Facebook and Steam rules for using their logo. We could try the logos intended for dark backgrounds so we can use dark buttons.

--- talk.php
+++ talk.php
@@ -12,12 +12,38 @@ mainMenu();
            </div>  
         
         <div class="content">
-            <div class="discordEmbed"><iframe src="https://discordapp.com/widget?id=223170046019633162&theme=dark" width="300px" height="474" allowtransparency="true" frameborder="0"></iframe></div>
+            <div class="discordEmbed"><iframe src="https://discordapp.com/widget?id=223170046019633162&theme=dark" width="300px" height="478" allowtransparency="true" frameborder="0"></iframe></div>
             
             <div class="group1">
-                <div class=groupIcon><a href="https://www.facebook.com/groups/135662413450556/"><div id="mega-facebook"></div></a></div>
-                <div class=groupIcon><a href="https://www.facebook.com/groups/31875193841/"><div id="dagd-facebook"></div></a></div>  
-                <div class=groupIcon><a href="https://steamcommunity.com/groups/fsu-dagd"><div id="dagd-steam"></div></a></div>
+                
+                <div class=groupIcon>
+                    <a href="https://www.facebook.com/groups/135662413450556/" target='_blank'>
+                        <div id="facebook">
+                            <p>Follow MEGA<br>
+                                on Facebook!
+                            </p>
+                        </div></a>
+                </div>
+                
+                
+                <div class=groupIcon>
+                    <a href="https://www.facebook.com/groups/31875193841/" target='_blank'>
+                        <div id="facebook">
+                            <p>Join DAGD<br>
+                                on Facebook!
+                            </p>
+                        </div></a>
+                </div>  
+                
+                
+                <div class=groupIcon>
+                    <a href="https://steamcommunity.com/groups/fsu-dagd" target='_blank'>
+                        <div id="steam">
+                            <p>Game with us<br>
+                                on STEAM!
+                            </p>
+                        </div></a>
+                </div>
             </div>
             
             <div class="group1">
