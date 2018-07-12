32afbb259466a06298f92684cc1e3c1d2380a236
Allen G <gardeia@ferris.edu>
2018-07-12 00:17:11 -0400

    404 Page
    Created basic 404 Page, basic for now but could be a fun side project later to put easter eggs on or something.
    
    -requires apache config update to load
    ErrorDocument 404 /404.php

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?
+    include "includes/templates.php";
+
+    beginPage("about");
+    mainMenu();
+?>
+
+<div class="tray">
+    <h1>ERROR 404 - PAGE NOT FOUND!</h1>
+</div>
+
+<div class="content">
+<h2>You are lost traveler... There is nothing for you here.</h2>
+    <p>How bout you check out some of the <a href="index.php">projects?</a>
+    </p>
+
+</div>
+
+<footer></footer>
+
+<? endPage(); ?>
